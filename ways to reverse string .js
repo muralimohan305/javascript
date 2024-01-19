6d87113e7451b41d44462791d3606910aac0bc12
muralimohan305 <58063997+muralimohan305@users.noreply.github.com>
2024-01-19 15:42:48 +0530

    Create ways to reverse string .js

---
+++ ways to reverse string .js
@@ -0,0 +1,23 @@
+
+
+1. Decrementing for-loop with concatenation
+function reverse(s){
+
+    var o='';
+    for(var i = s.length-1;i>=0;i--){
+
+        o += s[i];
+    }
+    return o;
+}
+
+let output = reverse("hello");
+console.log(output);
+
+
+2. In-built functions
+function reverse (s) {
+return s.split ('').reverse (j.join ("');
+}
+let output = reverse("hello");
+console.log(output);
