cf1a9de1882899f7a1280c9dd61ab5fd96223ad7
muralimohan305 <58063997+muralimohan305@users.noreply.github.com>
2024-02-07 12:40:26 +0530

    string.js

---
+++ string.js
@@ -0,0 +1,107 @@
+Code 1: Remove Duplicate characters from String
+Code 2: Remove Duplicate characters from array of element and find the count of an elements using set object
+Code 3: Remove Duplicate characters from array of element using filter
+Code 4:String reverse without reversing of individual words (Array of elements can be reverse with reverse() method but for string it is won't possible so required to split 
+and then join().
+Code 5:String reverse with reversing of individual words
+Code 6:String reverse without using inbult function
+Code 7: Find factorial of user input number
+Code 8:Anagram
+Code 9: Swapping of 2 numbers with third variable
+Code 10: Swapping of 2 numbers without third variable
+Code 11: To check the string or number is palindrome or not( ex: 121,madam,anna) using reverse method
+Code 12: To check the string or number is palindrome or not( ex: 121,madam,anna) using diving length by 2 and then comparing
+Code 13: To find longest word from a string using (for of) /*for(var i=0; i>=num; i++) means iterate by indexing*/  /*for (var word of words) means iterate by an elements not 
+Code 14: To find longest word from a string using functions
+Code 15: To find longest word from a string using custom code
+Code 16: To find longest common string from array of strings
+Code 17: To find vowels and its count in a given string
+Code 18:To find character occurance fro the string
+
+
+
+
+
+
+
+
+
+
+Code 19: To find a first pair whose sum is zero
+Code 20: To find a first pair whose sum is zero using indexing //Firstly do a sort here
+Code 21: To find the largest pair of the 2 elements using indexing with unsorted elements
+Code 22: To find the largest pair of the 2 elements using indexing with sorted elements
+Code 23: To find the index of an element from an array
+Code 24: Fibonacci Series (0,1,1,2,3,5,8,13....)
+Code 25: Fibonacci Series (0,1,1,2,3,5,8,13....) where keeping in array
+Code 26: Finding a missing elements in an array and then add with existing elements. (-1 means if elements not found then it will return always -1 as per rule)
+Code 27: Find the missing no. in an array
+Code 28: Sorting of an string/character
+Code 29: Sorting of an number
+Code 30: To check if given number is prime or not
+Code 31: To print all the numbers from 2 to 100
+Code 32: To find unique values from 2 arrays and keep into one array.
+Code 33: Find first duplicate element from an array
+Code 34: Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz". 
+For numbers which are multiples of both three and five, print "FizzBuzz"
+Code 35: Uppercase of each first letter of a words 
+Code 36: Uppercase of each first letter of a words using map function
+Code 37: To check ending of the string with given character/s using inbuild function
+Code 38: To check ending of the string with given character/s using custom
+Code 39: To find the largest elements fro the 2 dimensional array 
+Code 40: To find the largest elements fro the 2 dimensional array in another way
+Code 41: Print string n times using inbuilt function
+Code 42: Print string n times in custom way
+Code 43:Print string n times in custom way
+Code 44: Truncate the string
+
+console.log(truncateString("priyabagde",2)) //pr
+console.log(truncateString("priyabagde",4)) //p... //retuen only 4 digits
+
+Code 45: Converting one dimensional array into n dimensional array using slice
+console.log (chunkArrayInGroup(['a','b','c','d'],2)) //[["a", "b"], ["c", "d"]]
+Code 46: Converting one dimensional array into n dimensional array using splice
+console.log (chunkArrayInGroup(['a','b','c','d'],2)) //[["a", "b"], ["c", "d"]]
+Code 47: To find only truthy values
+Code 49:  To find only truthy values using filter
+
+removeFalseValue(["priya", 0 ,"", false, null,undefined, "ate", 9 ]) o/p:  ['priya', 'ate', 9]
+Code 50: Checking all letters of second words should present in first word, in the same order using include function
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
